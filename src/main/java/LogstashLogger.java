fcdec874a56f5a49a4952c39e13968669aaabe27
Jaap Stelwagen <jaapstelwagen@gmail.com>
2016-09-22 09:23:20 +0200

    Socket resource bug fixed

--- src/main/java/LogstashLogger.java
+++ src/main/java/LogstashLogger.java
@@ -1,32 +1,25 @@
-import java.io.Closeable;
 import java.io.IOException;
 import java.net.*;
 
 /**
  * Created by Jaap on 26-5-2016.
  */
-public class LogstashLogger implements Closeable {
+public class LogstashLogger {
 
     public final static LogstashLogger INSTANCE = new LogstashLogger();
 
     InetAddress host;
     final int port;
-    DatagramSocket socket;
 
     private LogstashLogger() {
         final Properties properties = new Properties();
         port = Integer.parseInt(properties.prop.getProperty("logstash.port"));
-        try {
-            socket = new DatagramSocket();
-        } catch (SocketException e) {
-            e.printStackTrace();
-        }
+
         try {
             host = InetAddress.getByName(properties.prop.getProperty("logstash.ip"));
         } catch (UnknownHostException e) {
             e.printStackTrace();
         }
-        addShutdownHook();
     }
 
     public void message(String line) {
@@ -37,7 +30,7 @@ public class LogstashLogger implements Closeable {
         send(who + ": " + line);
     }
     private void send(String message) {
-        if (socket != null) {
+        try (DatagramSocket socket =new DatagramSocket()){
             byte[] data = message.getBytes();
             try {
                 DatagramPacket packet = new DatagramPacket(data, data.length, host, port);
@@ -46,22 +39,8 @@ public class LogstashLogger implements Closeable {
                 System.out.println("ERROR for UDP connection " + socket.isConnected() + ", @"
                         + host.getHostAddress() + ":" + port + ", socket " + socket.isBound() + ". For " + message);
             }
-        } else {
-            System.out.println("Socket for LogStash failed to initialize, cannot send message " + message);
+        } catch (SocketException e) {
+            e.printStackTrace();
         }
-    }
-
-    @Override
-    public void close() {
-        socket.close();
-    }
-
-    private void addShutdownHook() {
-        Runtime.getRuntime().addShutdownHook(new Thread() {
-            @Override
-            public void run() {
-                close();
-            }
-        });
     }
 }
